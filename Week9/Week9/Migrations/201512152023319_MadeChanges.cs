34a3741b5d290f6a921d0c8e39ff2276a5223ca8
Justin Wyatt <justin@wyattcorporation.com>
2015-12-16 09:07:00 -0600

    Added Roles. Modified registration view.

---
+++ Week9/Week9/Migrations/201512152023319_MadeChanges.cs
@@ -0,0 +1,16 @@
+namespace Week9.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MadeChanges : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
